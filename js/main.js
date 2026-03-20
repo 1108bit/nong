ae10ab6d29f000edb5ae19f51ec6c56b7e874914
NONG[genka] <daumsda8612@gmail.com>
2026-03-20 23:29:27 +0900

    add

---
+++ js/main.js
@@ -0,0 +1,19 @@
+document.getElementById("logoutButton").addEventListener("click", () => {
+  alert("로그아웃 기능은 다음 단계에서 연결 예정");
+});
+
+document.getElementById("addCharacterButton").addEventListener("click", () => {
+  alert("캐릭터 추가 페이지는 다음 단계에서 연결 예정");
+});
+
+document.getElementById("goAvailabilityButton").addEventListener("click", () => {
+  alert("시간 등록 페이지는 다음 단계에서 연결 예정");
+});
+
+document.getElementById("goPartyButton").addEventListener("click", () => {
+  alert("파티 보기 페이지는 다음 단계에서 연결 예정");
+});
+
+document.getElementById("goHistoryButton").addEventListener("click", () => {
+  alert("내 기록 보기 페이지는 다음 단계에서 연결 예정");
+});
